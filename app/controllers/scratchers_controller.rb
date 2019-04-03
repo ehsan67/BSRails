28f208ee81e40f03e75430c3f348843c242532aa
Ehsan Eslamloueyan <ehs.esl67@yahoo.com>
2019-04-03 17:34:17 -0400

    Scratcher controller

---
+++ app/controllers/scratchers_controller.rb
@@ -0,0 +1,41 @@
+class ScratchersController < ApplicationController
+    before_action :set_scratcher, only: [:show, :update, :destroy]
+    def index
+        render json: Scratcher.all
+    end
+
+    def show
+        render json: @scratcher
+    end
+
+    def create
+        @scratcher = Scratcher.new(scratcher_params)
+        if @scratcher.save
+         render json: @scratcher, status: :created, location: scratcher_url(@scratcher)
+        else
+         render json: @scratcher.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        if @scratcher.update(scratcher_params)
+         render json: @scratcher
+        else
+         render json: @scratcher.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @scratcher.destroy
+    end
+    
+    private
+
+    def scratcher_params
+        params.require(:scratcher).permit(:name, :description, :size, :price)
+    end
+
+    def set_scratcher
+        @scratcher= Scratcher.find(params[:id])
+    end
+end
